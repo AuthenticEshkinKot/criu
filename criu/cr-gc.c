24a13e5f60b5296929981c4163ca4889f9c6c350
max <maxrider@mail.ru>
2016-05-16 20:04:00 +0300

    Unneeded funcs were removed

--- criu/cr-gc.c
+++ criu/cr-gc.c
@@ -10,9 +10,6 @@
 #include "sk-inet.h"
 #include "rst-malloc.h"
 
-static int gc_prepare_namespace();
-static int gc_prepare_sockets();
-
 int cr_garbage_collect(bool show)
 {
 	if (check_img_inventory() < 0)
@@ -20,23 +17,6 @@ int cr_garbage_collect(bool show)
 
 	if (collect_remaps_and_regfiles())
 		return -1;
-
-	if (gc_prepare_namespace())
-		return -1;
-
-	if (gc_prepare_sockets())
-		return -1;
-
-	gc_network(show);
-
-	gc_collected_remaps(show);
-
-	return 0;
-}
-
-static int gc_prepare_namespace()
-{  
-	pr_info("MAX: going to prepare_namespace\n");
 
 	task_entries = rst_mem_alloc(sizeof(*task_entries), RM_SHREMAP);
 
@@ -46,13 +26,12 @@ static int gc_prepare_namespace()
 	if (prepare_mnt_ns())
 		return -1;
 
-  return 0;
-}
-
-static int gc_prepare_sockets()
-{
 	if (collect_inet_sockets())
 		return -1;
+
+	gc_network(show);
+
+	gc_collected_remaps(show);
 
 	return 0;
 }
